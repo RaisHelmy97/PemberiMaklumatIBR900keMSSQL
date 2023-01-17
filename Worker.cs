c8a2746e9d9cc781ee758a497b8db2ddb2f6da2c
RaisHelmy97 <raishelmy1997@live.com>
2023-01-17 16:12:22 +0800

    default project

--- Worker.cs
+++ Worker.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace PemberiMaklumatIBR900keMSSQL
 {
     public class Worker : BackgroundService
@@ -14,6 +19,15 @@ namespace PemberiMaklumatIBR900keMSSQL
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                var client = new HttpClient();
+                var byteArray = Encoding.ASCII.GetBytes("admin:cradlepoint2020");
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                var responseGps = await client.GetAsync("http://10.192.23.1/api/status/gps/fix");
+                var contentGps = await responseGps.Content.ReadAsStringAsync();
+                Console.WriteLine("Content GPS: " + contentGps);
+                var responseGps = await client.GetAsync("http://10.192.23.1/api/status/stats/signal_history");
+                var contentGps = await responseGps.Content.ReadAsStringAsync();
+                Console.WriteLine("Content GPS: " + contentGps);
                 await Task.Delay(1000, stoppingToken);
             }
         }
